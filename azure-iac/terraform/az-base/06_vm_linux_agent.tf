4c2085bcbc756be56cdd76d2b25e962af83009ef
suren <suren>
2021-07-22 19:40:02 +0100

    linux agent vm redeploy

--- azure-iac/terraform/az-base/06_vm_linux_agent.tf
+++ azure-iac/terraform/az-base/06_vm_linux_agent.tf
@@ -1,38 +1,38 @@
-# module "linux-agent-vm" {
-#   source     = "../modules/vm/"
-#   res_prefix = local.res_prefix
-#   vm_prefix  = "linux-agent"
-#   loc        = var.loc
-#   rg_name    = azurerm_resource_group.base.name
+module "linux-agent-vm" {
+  source     = "../modules/vm/"
+  res_prefix = local.res_prefix
+  vm_prefix  = "linux-agent"
+  loc        = var.loc
+  rg_name    = azurerm_resource_group.base.name
 
-#   vm_count  = 1
-#   subnet_id = data.azurerm_subnet.vm.id
-#   vm_size   = "Standard_D2as_v4"
-#   os_disk = {
-#     caching              = "ReadWrite"
-#     storage_account_type = "StandardSSD_LRS"
-#     size_gb              = "50"
-#   }
+  vm_count  = 1
+  subnet_id = data.azurerm_subnet.vm.id
+  vm_size   = "Standard_D2as_v4"
+  os_disk = {
+    caching              = "ReadWrite"
+    storage_account_type = "StandardSSD_LRS"
+    size_gb              = "50"
+  }
 
-#   vm_admin = "suren"
-#   pub_key  = file("../files/id_rsa.pub")
-#   custom_data  = file("../files/cloud_init")
+  vm_admin = "suren"
+  pub_key  = file("../files/id_rsa.pub")
+  custom_data  = file("../files/cloud_init")
 
-#   # https://docs.microsoft.com/en-us/cli/azure/vm/image?view=azure-cli-latest
-#   image = {
-#     publisher = "Canonical"
-#     offer     = "0001-com-ubuntu-server-focal"
-#     sku       = "20_04-lts-gen2"
-#     version   = "latest"
-#   }
+  # https://docs.microsoft.com/en-us/cli/azure/vm/image?view=azure-cli-latest
+  image = {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-focal"
+    sku       = "20_04-lts-gen2"
+    version   = "latest"
+  }
 
-#   "os_disk" {  
-#     create_option       = "FromImage"
-#     caching              = "ReadWrite"
-#     storage_account_type = "StandardSSD_LRS"
-#     size_gb              = "50"
-#   }
+  "os_disk" {  
+    create_option       = "FromImage"
+    caching              = "ReadWrite"
+    storage_account_type = "StandardSSD_LRS"
+    size_gb              = "50"
+  }
 
-#   tags = local.tags
-# }
+  tags = local.tags
+}
 
