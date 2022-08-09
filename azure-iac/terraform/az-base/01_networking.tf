d8e48d345f6c1900478aa7adb3909aad58721e10
Suren <Suren>
2022-08-09 16:04:15 +0100

    terraform import test

--- azure-iac/terraform/az-base/01_networking.tf
+++ azure-iac/terraform/az-base/01_networking.tf
@@ -29,3 +29,43 @@ data "azurerm_subnet" "vm" {
   resource_group_name  = data.azurerm_resource_group.base.name
   depends_on           = [module.networking.vnet]
 }
+
+# # imported - ran import and updated terraform.tfvars (subnets added for cloudshell)
+# # terraform import azurerm_subnet.cloudshellsubnet /subscriptions/xxx-xxx/resourceGroups/azbase-weu/providers/Microsoft.Network/virtualNetworks/azbase-weu/subnets/cloudshellsubnet
+# resource "azurerm_subnet" "cloudshellsubnet" {
+#   name = "cloudshellsubnet"
+#   resource_group_name = data.azurerm_resource_group.base.name
+#   virtual_network_name = module.networking.vnet.name
+#     address_prefixes = ["10.0.220.0/26"]
+# enforce_private_link_endpoint_network_policies = true
+#    service_endpoints = [ "Microsoft.Storage"]
+#     delegation {
+#           name = "CloudShellDelegation" 
+
+#           service_delegation {
+#               actions = [
+#                   "Microsoft.Network/virtualNetworks/subnets/action",
+#               ]
+#               name    = "Microsoft.ContainerInstance/containerGroups"
+#           }
+#       }
+
+
+# }
+# #terraform import azurerm_subnet.relaysubnet /subscriptions/xxx-xxx/resourceGroups/azbase-weu/providers/Microsoft.Network/virtualNetworks/azbase-weu/subnets/relaysubnet
+# resource "azurerm_subnet" "relaysubnet" {
+#     name = "relaysubnet"
+#   resource_group_name = data.azurerm_resource_group.base.name
+#   virtual_network_name = module.networking.vnet.name
+#     address_prefixes = ["10.0.221.0/26"]
+#     enforce_private_link_endpoint_network_policies = true
+# }
+# #terraform import azurerm_subnet.storagesubnet /subscriptions/xxx-xxx/resourceGroups/azbase-weu/providers/Microsoft.Network/virtualNetworks/azbase-weu/subnets/storagesubnet
+# resource "azurerm_subnet" "storagesubnet" {
+#     name = "storagesubnet"
+#   resource_group_name = data.azurerm_resource_group.base.name
+#   virtual_network_name = module.networking.vnet.name
+#   address_prefixes = ["10.0.222.0/26"]
+# enforce_private_link_endpoint_network_policies = true
+#    service_endpoints = [ "Microsoft.Storage"]
+# }
